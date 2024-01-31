b33be5b1f55ae5484699390140e249e9ef47d9f6
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-01-31 16:35:20 +0100

    Add cart functionality to script.js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,11 @@
 let cartIcon = document.querySelector('#cart-icon');
 let cart = document.querySelector('.cart');
 let closeCart = document.querySelector('#close-cart');
+
+cartIcon.onclick = () => {
+    cart.classList.add('active');
+};
+
+closeCart.onclick = () => {
+    cart.classList.remove('active');
+};
