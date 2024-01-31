937e9198b14e0bf4ab3b9893961aa92a05872521
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-01-31 16:52:55 +0100

    feat: Add remove item and update total price functions
    Added two new functions in the script.js file to enhance the user's experience. The first function allows users to remove items from their cart. The second function automatically updates the total price in the cart after an item is removed.

--- assets/script.js
+++ assets/script.js
@@ -9,3 +9,39 @@ cartIcon.onclick = () => {
 closeCart.onclick = () => {
     cart.classList.remove('active');
 };
+
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready);
+} else {
+    ready();
+}
+
+function ready() {
+    // Remove products from cart
+    let removeCartButtons = document.getElementByClassName('cart-remove');
+    console.log(removeCartButtons);
+    for (let i = 0; i < removeCartButtons.length; i++) {
+        let button = removeCartButtons[i];
+        button.addEventListener('click', removeCartItem);
+    }
+}
+
+function removeCartItem(event) {
+    let buttonClicked = event.target;
+    buttonClicked.parentElement.remove();
+}
+
+// Update total price
+function updateTotalPrice() {
+    let cartContent = document.getElementByClassName('cart-content')[0];
+    let cartBoxes = cartContent.getElementByClassName('cart-box');
+    let total = 0;
+    for (let i = 0; i < cartBoxes.length; i++) {
+        let cartBox = cartBoxes[i];
+        let priceElement = cartBox.getElementByClassName('cart-price')[0];
+        let quantityElement = cartBox.getElementByClassName('cart-quantity')[0];
+        let price = parseFloat(priceElement.innerText.replace("", "€"));
+        let quantity = quantityElement.value;
+        total = total + price * quantity;
+    }
+}
